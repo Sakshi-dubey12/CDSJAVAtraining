0a7770875e9466b9418279e5949fa9b16f434b0a
unknown <ssakshidubey015@gmail.com>
2024-07-22 15:13:12 +0530

    day 2

---
+++ SumOfTwoNumber.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+public class SumOfTwoNumber {
+    public static void main(String[] args){
+        int a = 5;
+        float b = 7.5F;
+        float sum = a + b;
+        System.out.println(sum);
+    }
+}
