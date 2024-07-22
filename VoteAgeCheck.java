222707b75e6a297520c1c8db7d1d4d6de3aa8c85
unknown <ssakshidubey015@gmail.com>
2024-07-22 16:03:40 +0530

    Half traingle

--- VoteAgeCheck.java
+++ VoteAgeCheck.java
@@ -1,10 +1,14 @@
 public class VoteAgeCheck {
     public static void main(String[] args){
         int age = 20;
+        //with terminal
+        // with if else
         if(age > 18) {
             System.out.println("Eligible for voting");
         }else{
             System.out.println("Not Eligible for vote");
         }
+
+
     }
 }
