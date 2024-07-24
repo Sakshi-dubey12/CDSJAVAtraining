39c3e9e61f048e0f5526ad43e1ce6761c5cb1ac3
unknown <ssakshidubey015@gmail.com>
2024-07-24 19:29:10 +0530

    day5 Home Work

---
+++ sortingArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class sortingArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 7, 3, 11, 24, 9};
+
+        // Sort the array
+        Arrays.sort(arr);
+
+        // Print the sorted array
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+
+
