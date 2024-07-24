9e7877cda3bca4f9170a14eea61b358d4e09b3c0
unknown <ssakshidubey015@gmail.com>
2024-07-24 22:49:11 +0530

    Day5 Home work

---
+++ ReverseString.java
@@ -0,0 +1,21 @@
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String name = "Sakshi";
+        System.out.println(ReverseString(name));
+    }
+    public static char[] ReverseString(String name){
+        int start = 0;
+        char[] charArray = name.toCharArray();
+        int end = name.length() - 1;
+        while (end >= start ){
+            //swap char
+            char temp = charArray[start];
+            charArray[start] = charArray[end];
+            charArray[end] = temp;
+            end--;
+            start++;
+        }
+        return charArray;
+    }
+}
