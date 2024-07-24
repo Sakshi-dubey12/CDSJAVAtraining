0bde9c9f5db72698d0a65731c6e35c476ed8f684
unknown <ssakshidubey015@gmail.com>
2024-07-24 14:34:04 +0530

    day5

---
+++ ContinueStatement.java
@@ -0,0 +1,12 @@
+public class ContinueStatement {
+    public static void main(String[] args){
+        for(int i =1; i<=10; i++) {
+            if(i==6){
+              //  break;
+                continue;
+            }
+            System.out.println(i);
+
+        }
+    }
+}
