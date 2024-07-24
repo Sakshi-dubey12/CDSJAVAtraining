af165ab41413805d279bea2e45828f03d821cddc
unknown <ssakshidubey015@gmail.com>
2024-07-24 15:52:04 +0530

    day5

---
+++ ForEachLoop.java
@@ -0,0 +1,9 @@
+public class ForEachLoop {
+    public static void main(String[] args){
+        String[] friendNames = {"Ram","Lakshman","Krishna","Vishnu","hamuman"};
+        for(String name : friendNames){
+            System.out.print(name + " ");
+        }
+
+    }
+}
