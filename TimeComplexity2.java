aad71fabecf629bfbdfed3e55502bd20aa930795
unknown <ssakshidubey015@gmail.com>
2024-07-25 15:00:09 +0530

    Day6

---
+++ TimeComplexity2.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+class GFG {
+    public static void main(String args[]){
+        int i, n = 8;
+        for (i = 2; i <= n; i=(int)Math.pow(i,2)) {
+            System.out.println("Hello World !!!");
+        }
+    }
+}
+
+
---
+++ TimeComplexsityPrograms.java
@@ -0,0 +1,11 @@
+public class TimeComplexsityPrograms {
+    public static void main(String[] args){
+                int i, n = 12;
+                for (i = 1; i <= n; i=i*2) {
+                    System.out.println("Hello World !!!");
+                    //n =8,iteration =4 , when n = 12, iteration =4
+                    // when n = 16 iteration =5
+                }
+            }
+        }
+
