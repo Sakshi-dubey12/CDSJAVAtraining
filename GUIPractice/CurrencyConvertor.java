7308deb2e6870faaa26abf604fed0bf50147ce57
unknown <ssakshidubey015@gmail.com>
2024-07-29 22:47:48 +0530

    DAY 9

--- GUIPractice/CurrencyConvertor.java
+++ GUIPractice/CurrencyConvertor.java
@@ -1,57 +1,73 @@
 package GUIPractice;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CurrencyConvertor {
     public static void main(String[] args) {
-        //currency convertor
+        currencyConvertor();
+    }
+
+    private static void currencyConvertor() {
         JFrame frame = new JFrame("Currency Convertor");
+        JLabel rsLabel = new JLabel("Enter amount in Rs: ");
+        JLabel usdLabel = new JLabel("Enter amount in USD: ");
+        JTextField rsField = new JTextField();
+        JTextField usdField = new JTextField();
+        JButton button = new JButton("Convert");
+        JLabel result = new JLabel();
 
-        //user to display text msg to user
-        JLabel USDLabel = new JLabel("Enter your USD");
-        JLabel INRLabel = new JLabel("Enter your INR");
-        JTextField USDField = new JTextField();
-        JTextField INRField = new JTextField();
-        JButton ConvertorButton = new JButton("Convert Currency");
-        JLabel resultLabel = new JLabel();
-
-        //to set the position of button of UI component in frame
-        USDLabel.setBounds(30,30,130,40);
-        USDField.setBounds(180,30,50,40);
-        INRLabel.setBounds(30,70,150,40);
-        INRField.setBounds(180,70,50,40);
-        ConvertorButton.setBounds(30,170,200,40);
-        resultLabel.setBounds(30,170,200,40);
-
-        //t0 attach the Ui component to frame
-        frame.add(resultLabel);
-        frame.add(ConvertorButton);
-        frame.add(INRLabel);
-        frame.add(USDLabel);
-        frame.add(USDField);
-        frame.add(INRField);
-        frame.add(resultLabel);
-
-        //to set layout null
+        rsLabel.setBounds(30, 30, 150, 40);
+        usdLabel.setBounds(30, 80, 150, 40);
+        rsField.setBounds(180, 30, 50, 40);
+        usdField.setBounds(180, 80, 50, 40);
+        button.setBounds(30, 130, 100, 40);
+        result.setBounds(140, 130, 250, 40);
+        frame.add(result);
+        frame.add(button);
+        frame.add(rsLabel);
+        frame.add(usdLabel);
+        frame.add(rsField);
+        frame.add(usdField);
         frame.setLayout(null);
-        frame.setSize(300,300);
+        frame.setSize(500, 250);
         frame.setVisible(true);
-        // to click on the Convertor
-        ConvertorButton.addActionListener(new ActionListener() {
+
+        button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                float  USD_price = 87.5f;
-                float ans = 0;
-                float USDValue = Float.parseFloat(USDField.getText());
-                ans = USDValue*USD_price;
-                resultLabel.setText(String.valueOf(USDValue));
-//                CurrencyConvertor.setText(String.valueOf(USDValue));
 
+                //case 1 when user input in rs and usd is empty
+                if (!rsField.getText().isEmpty()
+                        && usdField.getText().isEmpty())
+                {
+                    float rs = Float.parseFloat(rsField.getText());
+                    float inrToUsd = (float) (rs/83.73);
+                    result.setText("INR to usd :"+ String.valueOf(inrToUsd));
+                } else if (rsField.getText().isEmpty()
+                        && !usdField.getText().isEmpty()) {
+                    float usd = Float.parseFloat(usdField.getText());
+                    float usdToInr = (float) (usd* 83.73);
+                    result.setText("Usd to Inr :" + usdToInr);
+                } else if (rsField.getText().isEmpty()
+                        && usdField.getText().isEmpty()) {
+                    result.setText("Enter atlease one amount to convert :" );
+                }
+                else {
+                    float rs = Float.parseFloat(rsField.getText());
+                    float inrToUsd = (float) (rs/83.73);
+                    float usd = Float.parseFloat(usdField.getText());
+                    float usdToInr = (float) (usd* 83.73);
+                    result.setText("In usd :"+ String.valueOf(inrToUsd) +
+                            "In INR"+ usdToInr
+                    );
+                }
 
-
+                //case 2 when user input in usd
+//                float usdToInr = (float) (usd* 83.73);
+//                result.setText("Usd to Inr :" + usdToInr);
             }
         });
     }
-
 }
